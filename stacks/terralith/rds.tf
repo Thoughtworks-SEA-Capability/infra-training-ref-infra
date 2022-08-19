7335a53a67f1017d5fa8ed2633e32eb395fb40fd
Vinod Dampuru <vinod.dampuru@thoughtworks.com>
2022-08-20 07:11:55 +0800

    Making rds as module

--- stacks/terralith/rds.tf
+++ stacks/terralith/rds.tf
@@ -1,54 +1,17 @@
 locals {
   db_name = "${local.name}-app-a-db"
 }
-module "cluster" {
-  source  = "terraform-aws-modules/rds-aurora/aws"
-  version = "7.2.1"
 
-  name           = local.db_name
-  engine         = "aurora-postgresql"
-  engine_version = "11.12"
+module "app_aurora_db" {
+  source = "../../modules/rds"
+
+  db_name = local.db_name
   instance_class = "db.t3.medium"
-  instances = {
-    one = {}
-  }
-
-  vpc_id  = module.vpc.vpc_id
-  create_db_subnet_group = false
-  db_subnet_group_name = module.vpc.database_subnet_group_name
-  subnets = module.vpc.database_subnets
-
-  allowed_security_groups = [module.eks.cluster_primary_security_group_id]
-  allowed_cidr_blocks     = ["10.0.0.0/20"]
-
-  storage_encrypted   = true
-  apply_immediately   = true
-  monitoring_interval = 10
-
-  db_parameter_group_name         = aws_db_parameter_group.db.name
-  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.db.name
-
-  enabled_cloudwatch_logs_exports = ["postgresql"]
-
-  tags = merge(local.tags,{
-    Name = local.db_name
-  } )
-}
-
-resource "aws_db_parameter_group" "db" {
-  name = local.db_name
-  family = "aurora-postgresql11"
-  tags = merge(local.tags, {
-    Name: local.db_name
-  })
-}
-
-resource "aws_rds_cluster_parameter_group" "db" {
-  name = local.db_name
-  family = "aurora-postgresql11"
-  tags = merge(local.tags, {
-    Name: local.db_name
-  })
+  vpc_id = module.vpc.vpc_id
+  database_subnet_group_name = module.vpc.database_subnet_group_name
+  database_subnets = module.vpc.database_subnets
+  eks_security_group_ids = [module.eks.cluster_primary_security_group_id]
+  tags = local.tags
 }
 
 // This secret name is sort of like config store for infra to pass on data to the application layer
@@ -59,9 +22,9 @@ resource "kubernetes_secret_v1" "app-a-rds-creds" {
     namespace = local.application-ns-name
   }
   data = {
-    db_name = module.cluster.cluster_database_name,
-    db_endpoint = module.cluster.cluster_endpoint,
-    db_username = module.cluster.cluster_master_username,
-    db_password = module.cluster.cluster_master_password,
+    db_name = module.app_aurora_db.cluster_database_name,
+    db_endpoint = module.app_aurora_db.cluster_endpoint,
+    db_username = module.app_aurora_db.cluster_master_username,
+    db_password = module.app_aurora_db.cluster_master_password,
   }
 }
