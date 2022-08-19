dcf2a32e8055aaf637f691a61a60fbf88e1371d5
Vinod Dampuru <vinod.dampuru@thoughtworks.com>
2022-08-20 07:42:26 +0800

    Minor refactoring to eks

--- stacks/terralith/eks.tf
+++ stacks/terralith/eks.tf
@@ -31,37 +31,6 @@ module "app_eks" {
   tags               = local.tags
 }
 
-moved {
-  from = module.eks
-  to = module.app_eks.module.eks
-}
-
-moved {
-  from = module.vpc_cni_irsa
-  to = module.app_eks.module.vpc_cni_irsa
-}
-
-moved {
-  from = aws_iam_role_policy_attachment.additional
-  to = module.app_eks.aws_iam_role_policy_attachment.additional
-}
-
-moved {
-  from = aws_kms_key.eks
-  to = module.app_eks.aws_kms_key.eks
-}
-
-moved {
-  from = aws_iam_policy.node_additional
-  to = module.app_eks.aws_iam_policy.node_additional
-}
-
-moved {
-  from = aws_iam_role.eks-admin
-  to = module.app_eks.aws_iam_role.eks-admin
-}
-
-
 // This namespace name is agreed by convention between the infra and app layers
 // Changing the name will break the test and subsequently whatever application.
 locals {
